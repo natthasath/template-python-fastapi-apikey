d6879a2bb9cc7fc2b9da1d08f204f6623a529e71
natthasath <natthasath.sak@gmail.com>
2024-06-13 13:38:54 +0700

    Update .env

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import uvicorn
 from decouple import config
 
 if __name__ == "__main__":
-    # uvicorn.run("app.api:app", host=str(config("APP_HOST")), port=int(config("APP_PORT")), reload=True)
-    uvicorn.run("app.api:app", host=str(config("APP_HOST")), port=int(config("APP_PORT")), reload=True, ssl_certfile=config("SSL_CERTIFICATE"), ssl_keyfile=config("SSL_KEYFILE"))
+    uvicorn.run("app.api:app", host=str(config("APP_HOST")), port=int(config("APP_PORT")), reload=True)
+    # uvicorn.run("app.api:app", host=str(config("APP_HOST")), port=int(config("APP_PORT")), reload=True, ssl_certfile=config("SSL_CERTIFICATE"), ssl_keyfile=config("SSL_KEYFILE"))
